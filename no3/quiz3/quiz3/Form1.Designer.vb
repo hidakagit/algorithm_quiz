40731bdc3603dede268b8c76acfe8fbd195270a8
hidakagit <h7g4bin@gmail.com>
2014-02-27 16:18:00 +0900

    question3(fin)

---
+++ no3/quiz3/quiz3/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(297, 80)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "パターン表示"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Meiryo UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 109)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(360, 141)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 46)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 19)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "括弧組数"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "パターン総数: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(88, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 12)
+        Me.Label3.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 262)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ no3/quiz3/quiz3/Form1.vb
@@ -0,0 +1,81 @@
+﻿Public Class Form1
+
+    Public number As Integer '括弧の組数
+    Public pattrn() As String '括弧総パターン
+    Public good() As String '正しい括弧総パターン
+
+    Private Sub 表示初期化()
+        RichTextBox1.Clear()
+        Label3.Text = ""
+    End Sub
+
+    Private Sub 括弧組数確定(sender As Object, e As EventArgs) Handles Button1.Click
+        number = Val(TextBox1.Text)
+        If number = 0 Then
+            MsgBox("1以上の整数を入力してください")
+            Exit Sub
+        End If
+
+        Call 表示初期化()
+        Call 括弧組み合わせ書き出し()
+        Call 括弧不当組み合わせ除去()
+
+        Dim outputstring As String = vbNullString
+        For i As Integer = 0 To good.Length - 1
+            If i = 0 Then
+                outputstring = good(i)
+            Else
+                outputstring = outputstring & " ," & good(i)
+            End If
+        Next
+        If outputstring Is vbNullString Then
+            MsgBox("表示できる括弧パターンがありません")
+            Exit Sub
+        End If
+        RichTextBox1.Text = outputstring
+        Label3.Text = good.Length
+    End Sub
+
+    '0->「(」, 1->「)」
+    Public Function 括弧選択(ByVal str As String) As String
+        Dim strtmp As String = vbNullString
+        strtmp = str.Replace("0", "(").Replace("1", ")")
+        Return strtmp
+    End Function
+
+    Public Sub 括弧組み合わせ書き出し()
+        Dim kc As Long = 0 '存在カウンター
+        For i As Long = 0 To 2 ^ (2 * number) - 1
+            Dim knum As String = Convert.ToString(CInt(i), 2).PadLeft(2 * number, "0"c)
+            If (knum.Length - knum.Replace("1", "").Length = number) Then
+                ReDim Preserve pattrn(kc)
+                pattrn(kc) = knum
+                kc = kc + 1
+            End If
+        Next
+    End Sub
+
+    Public Sub 括弧不当組み合わせ除去()
+
+        Dim kc As Integer = 0 'カウンター
+
+        For i As Integer = 0 To pattrn.Length - 1
+            Dim kpattrn As String = pattrn(i)
+            Dim ktmp As String = kpattrn 'tmp
+            Dim cc As Integer = 0 'パターンが見つからない時に抜けるためのカウンタ
+            While Not (ktmp.Length <= 2)
+                If InStr(kpattrn, "01") Then
+                    ktmp = ktmp.Replace("01", "")
+                End If
+                cc = cc + 1
+                If cc > 1 Then Exit While
+            End While
+            If ktmp = "01" Or ktmp = "" Then
+                ReDim Preserve good(kc)
+                good(kc) = 括弧選択(kpattrn)
+                kc = kc + 1
+            End If
+        Next
+    End Sub
+
+End Class
